fc3c408bd2254f44bdca23e88b2d598f57816098
Abdullah Bawazer <fn_nofe@yahoo.com>
2024-03-01 11:57:23 +0400

    Updated Observer

---
+++ Assets/Abdullah/Script/Observer/Listner.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Listner : MonoBehaviour
+{
+    public bool indvisualResponse=false;
+    public GameEvent Event;
+    public UnityEvent Response;
+    
+    private void OnEnable()
+    {
+        Event.AddListener(this);
+    }
+    private void OnDisable()
+    {
+        Event.RemoveListener(this);
+    }
+
+    public void EventRaised()
+    {
+        Response.Invoke();
+    }
+
+
+
+}
