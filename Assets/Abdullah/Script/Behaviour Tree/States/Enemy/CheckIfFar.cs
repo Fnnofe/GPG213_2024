932c42e5edf92a955da0b7c504b392789e5ac639
Abdullah Bawazer <fn_nofe@yahoo.com>
2024-04-15 13:13:38 +0400

    _UI Texture

--- Assets/Abdullah/Script/Behaviour Tree/States/Enemy/CheckIfFar.cs
+++ Assets/Abdullah/Script/Behaviour Tree/States/Enemy/CheckIfFar.cs
@@ -35,6 +35,7 @@ public class CheckIfFar : TreeNode
             //TooFar 
             //despawn
             Debug.Log("DespawnEnemy");
+            player.gameObject.SetActive(false);
             return NodeState.Failure;
 
         }
